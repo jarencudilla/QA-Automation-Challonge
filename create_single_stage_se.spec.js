382d5f91e036783d15459b0f3c52f0d49e847c56
Jaren Cudilla <jarencudilla@gmail.com>
2022-06-20 19:47:30 +0800

    Update create_single_stage_se.spec.js

--- create_single_stage_se.spec.js
+++ create_single_stage_se.spec.js
@@ -1,4 +1,7 @@
 describe('Create Single Stage Tournaments', function () {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
 
   it('Login to Master Staging', function (){
      cy.visit("https://master.challonge.online/user_session/new?continue=%2F");
@@ -24,7 +27,36 @@ describe('Create Single Stage Tournaments', function () {
     cy.get(".form-group > #user_session_password")
       .type('12345678')
     cy.get(".form-group > .btn")
-      .click()  
+      .click()
+    cy.get('#tournament_name')
+      .type('Cypress Test - SE')
+    cy.get('.trumbowyg-editor')
+      .type('Cypress Test - SE')
+    cy.get('#tournament_game_name')
+      .type('Battle City')
+    cy.get('.single-stage > #tournament_tournament_type')
+      .select('Single Elimination')
+    cy.get('#mark_as_tentative')
+      .click()
+    cy.get('.right > .btn')
+      .click()
+    // cy.wait(5000)
+    // cy.get(':nth-child(8) > .link')
+    // cy.get('.chl-text')
+    cy.get(".tabbed-navlist")
+      .contains("Participants")
+      .click({force: true})
+    cy.get('.clickable')
+      .click()
+    cy.get('.controls > .form-control')
+      .type('Test 1{enter}')
+      .type('Test 2{enter}')
+      .type('Test 3{enter}')
+      .type('Test 4{enter}')
+      .type('Test 5{enter}')
+      .type('Test 6{enter}')
+    cy.get('.control-group > .pull-right > .btn')
+      .click()
   });
 
  });
