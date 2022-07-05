7713c261b15a6279a2cbfe673dcd77dc7afd2896
Jaren Cudilla <jarencudilla@gmail.com>
2022-07-05 19:51:57 +0800

    create single stage test for Round Robin

--- create_single_stage_rr.spec.js
+++ create_single_stage_rr.spec.js
@@ -1,1 +1,11 @@
-// Single Stage Round Robin 
+///<reference types="Cypress" />
+
+describe('Create Single Stage Tournaments', function () {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+  it('Login to Master Staging', function (){
+    cy.login('Jaren', '12345678')
+  });
+
+  // Single Stage Round Robin
