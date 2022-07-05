f6b1c23da834b238b03077965e9caeeefe718da7
Jaren Cudilla <jarencudilla@gmail.com>
2022-07-05 19:51:40 +0800

    create single stage test for Leaderboard

---
+++ create_single_stage_lb.spec.js
@@ -0,0 +1,11 @@
+///<reference types="Cypress" />
+
+describe('Create Single Stage Tournaments', function () {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+  it('Login to Master Staging', function (){
+    cy.login('Jaren', '12345678')
+  });
+
+//Single Stage Leaderboard
